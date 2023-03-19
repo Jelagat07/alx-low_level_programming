e67e86b9d3f0aa6fe7c4c323189382d01d22886c
Jelagat07 <kimosop29@gmail.com>
2023-03-19 04:49:37 -0700

    display a character string with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) /**
+		 *This is the entry point
+		 *always returns 0 if successful
+		 **/
 {
 	/*string initialization*/
-	char progString[] = "\"Programming is like building a multilingual puzzle";
-	puts(progString)
+	char progString[] = "Programming is like building a multilingual puzzle";
 
+	puts(progString\n);
 	return (0);
 }
