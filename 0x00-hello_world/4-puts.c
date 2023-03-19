48aee3ae0d8d838516da3fbf766ccdb6364b5a4d
Jelagat07 <kimosop29@gmail.com>
2023-03-19 04:23:42 -0700

    use puts to output on Standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*string initialization*/
+	char progString[] = "\"Programming is like building a multilingual puzzle";
+	puts(progString)
+
+	return (0);
+}
