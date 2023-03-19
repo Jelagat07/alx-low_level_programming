3af0cc88dffb0f9c1b412377d7262e6ecf87415c
Jelagat07 <kimosop29@gmail.com>
2023-03-19 12:47:21 -0700

    display a sentence using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main: entry point
+ *
+ * Description: this program displays a character string using printf
+ *
+ * Return: Always returns 0(success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		Return(0);
+}
