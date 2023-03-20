d475a85b17da2194c443b1bc3cc64017b09fc1c1
Jelagat07 <kimosop29@gmail.com>
2023-03-20 14:21:09 -0700

    got rid of description and lowercased return

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,10 @@
 /**
  * main - Entry point
  *
- * Description: this program displays a character string using printf
- *
  * Return: Always 0(success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		Return(0);
+	return(0);
 }
