5109fc72e63476a0ceb82d09ebae3bb910902046
Jelagat07 <kimosop29@gmail.com>
2023-03-24 22:55:00 +0000

    used putchar to display letters of alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 if successful
+ * while: performs action until a condition is mer
+ */
+
+int main(void)
+{
+	char lower_letter = 'a';
+
+	while (lower_letter <= 'z')
+	{putchar (lower_letter);
+	lower_letter++;
+	putchar ('\n');
+	return (0);
+}
+
