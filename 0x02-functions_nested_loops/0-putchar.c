43c1389239d3184b8821dc0f75824bdccf210a2f
Jelagat07 <kimosop29@gmail.com>
2023-03-25 19:48:53 +0000

    display putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0(succsess)
+ * putchar: function to output values
+ */
+int main(void)
+{
+	putchar("_putchar\n")
+	return (0);
+}
