6afac463831cec3b63e137fdd01f044ad1b82d89
Jelagat07 <kimosop29@gmail.com>
2023-03-29 18:07:39 +0000

    used separate for loops for Alphabets and numerics

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,21 +2,22 @@
 /**
  * main - prints all the numbers of base 16 in lowercase, followed
  * by a new line
- * Return: Always 0 (Success)
+ * Return: Always 0(Success)
  * @int: an integer
  */
-int putchar(int);
 int main(void)
 {
-	int n;
-	char ch;
+	int i;
+	int k;
 
-	for (n = 12; n < 22; n++)
+	for (i = 48; i <= 58; i++)
 	{
-	for (ch = 'a'; ch <= 'f'; ch++)
+	putchar(i);
+	}
+	for (k = 97; k <= 102; k++)
 	{
-	putchar(ch);
-	}}
+	putchar(k);
+	}
 	putchar('\n');
 	return (0);
 }
