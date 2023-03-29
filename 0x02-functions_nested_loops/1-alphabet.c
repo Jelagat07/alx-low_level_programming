020acb8fa1f1ccc3cd327f026f80a1f0137aa82b
Jelagat07 <kimosop29@gmail.com>
2023-03-29 22:08:05 +0000

    use while loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,14 @@
  * print_alphabet - prints alphabet in lowercase followed by newline
  * Return: Always 0
  */
-void print_alphabet(void); /*prototype*/
-int main(void)
+void print_alphabet(void) /*prototype*/
 {
-	int n;
+	char ch = 'a';
 
-	for (n = 97; n <= 122; n++)
+	while (ch <= 'z')
 	{
-	_putchar(n);
+	_putchar(ch);
+	ch++;
 	}
 	_putchar('\n');
 }
