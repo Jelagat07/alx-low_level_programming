56f608a1decfc2419fd4b7a8f565ec7dd7cd0547
Jelagat07 <kimosop29@gmail.com>
2023-03-30 21:21:14 +0000

    display every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/** */
+void jack_bauer(void)
+{
+	int h, m;
+
+	while (h < 24)
+	{
+	m = 0;
+	while (m < 60)
+	{
+	_putchar((h / 10) + '0');
+	_putchar((h % 10) + '0');
+	_putchar(':');
+	_putchar((m / 10) + '0');
+	_putchar((m % 10) + '0');
+	_putchar('\n');
+	m++;
+	}
+	h++;
+	}
+}
