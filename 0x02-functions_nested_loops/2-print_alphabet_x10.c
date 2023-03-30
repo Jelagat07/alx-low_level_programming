a73ba724d784a1ee091f53a39a4e5b4e94aa9f9d
Jelagat07 <kimosop29@gmail.com>
2023-03-30 10:39:11 +0000

    included the _putchar function in main.h

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * print_alphabet - print the alphabet in lowercase ten times
- * _putchar - a fucntion that prints characters
+ * print_alphabet_x10 - print the alphabet in lowercase ten times
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
 	char ch;
 	int k = 0;
 
-	while (k++ <= 9)
+	while (k <= 9)
 	{
 	for (ch = 'a'; ch <= 'z'; ch++)
 	_putchar(ch);
 	_putchar('\n');
+	k++;
 	}
 }
 
