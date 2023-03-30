2acff303d2350b09f16cf4b69b6f78d0b4f0083d
Jelagat07 <kimosop29@gmail.com>
2023-03-30 22:20:04 +0000

    sum up two figures

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - sums up two integers
+ * Return: an integer
+ * @x: an integer parameter
+ * @y: an integer parameter
+ */
+int add(int x, int y)
+{
+	int r;
+
+	(r = x + y);
+	return (r);
+}
