0c936b7a77b8f7d51ba6d37413b5770cd3977044
Jelagat07 <kimosop29@gmail.com>
2023-03-30 20:43:29 +0000

    display last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digitdisplays last figure of a number
+ * Description: uses the modulo 10 to find the last digit
+ * @n: an integer parameter
+ * Return: an integer
+ */
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	last = n % 10;
+	{
+	_putchar(last);
+	}
+	return(0);
+}
