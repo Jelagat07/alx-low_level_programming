74ebc54a684683355fce58a595599202429e3d97
Jelagat07 <kimosop29@gmail.com>
2023-03-30 22:09:03 +0000

    corrected use of row in place of column in the for statemenets

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-	for (row = 0; column <= 9; column++)
+	for (column = 0; column <= 9; column++)
 	{multi = row * column;
 	x = multi / 10;
 	y = multi % 10;
