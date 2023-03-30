cd0a7929304b13a190e6c3b724143f796f6bc711
Jelagat07 <kimosop29@gmail.com>
2023-03-30 20:10:28 +0000

    print the absolute value of a signed int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value
+ * Return: Always a positive integer
+ * @k: an integer
+ */
+int _abs(int k)
+{
+	int r;
+
+	if (k >= 0)
+	{(r = k * 1);
+	return (r);
+	}
+	else
+	{(r = k * -1);
+	return (r);
+	}
+}
