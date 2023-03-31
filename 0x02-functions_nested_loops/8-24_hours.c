d44bf7ecdcbefca33b47af82ecbd48b4217c590b
Jelagat07 <kimosop29@gmail.com>
2023-03-31 14:11:41 +0000

    corrected order

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,14 +9,12 @@ void jack_bauer(void)
 
 	while (h < 24)
 	{
-	h_tens = ((h % 100) / 10);
-	h_ones = (h % 10);
-	_putchar(h_tens + '0');
-	_putchar(h_ones + '0');
-	_putchar(':');
 	while (m < 60)
 	{m_tens = ((m % 100) / 10);
 	m_ones = (m % 10);
+	_putchar(h_tens);
+	_putchar(h_ones);
+	_putchar(':');
 	_putchar(m_tens + '0');
 	_putchar(m_ones + '0');
 	_putchar('\n');
