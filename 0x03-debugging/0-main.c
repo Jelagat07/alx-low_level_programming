c4288e523db38b9341dbcfa8813ab42795e0e16d
myusername <kimosop29@gmail.com>
2023-03-31 12:26:43 +0000

    debug a function to determine sign of  number

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * postitive_or_negative: determines the status of an input
+ * Return: Always 0 if successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	postitive_or_negative(i)
+	return (0);
+}
