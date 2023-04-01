1f10cf0486b87bdca7926bc965375f4cc9d0b72d
Jelagat07 <kimosop29@gmail.com>
2023-04-01 14:18:50 +0000

    corrected spelling

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * main - Entry point
- * postitive_or_negative: determines the status of an input
+ * positive_or_negative: determines the status of an input
  * Return: Always 0 if successful
  */
 int main(void)
 {
 	int i = 0;
 
-	postitive_or_negative(i)
+	positive_or_negative(i);
 	return (0);
 }
