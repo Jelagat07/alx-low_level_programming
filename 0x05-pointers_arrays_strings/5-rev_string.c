308a2b333b2ccb9d8f21a717d884227426e10ca0
Jelagat07 <kimosop29@gmail.com>
2023-04-02 14:56:41 +0000

    print character string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * Return: void
+ * @s: a character string
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+
+	if (s[i] != '\0')
+		i++;
+	for (j = i - 1; j >= 0; j--)
+	{
+	_putchar(s[j]);
+	}
+}
