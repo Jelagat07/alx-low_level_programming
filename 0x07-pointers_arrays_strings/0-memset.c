3deb0a4820de8d883cc5a5aad26a0d8f33e77ec4
Jelagat07 <kimosop29@gmail.com>
2023-04-03 21:22:42 +0000

    constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * * _memset - fills memory with a constant byte
+ * *_memset - fills memory with a constant byte
  * @s: memory area to be filled
  * @b: character to be copied
  * @n: number of bytes to be set
+ * Return: pointer to memory area
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int z = 0;
+	unsigned int z;
 
-	while (z < n)
+	for (z = 0; z < n; z++)
 	{
-		s[z] = b;
-		z++;
+	s[z] = b;
 	}
 	return (s);
 }
